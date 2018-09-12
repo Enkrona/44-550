90b3c676471cc2dd2bb581bd84833cb2e5d8eb79
Brad Taylor <brad@enkrona.net>
2018-09-12 14:39:39 -0400

    cont

--- header.h
+++ header.h
@@ -1,4 +1,11 @@
+//  header guard -- also known as precopiler directives
+#ifndef HEADER_H
+#define HEADER_H 
+
 //  funtion prototypes
 int foo();
 void greet(int id);
 int bar();
+
+//  Keeps compiler from redefining header.h 
+#endif //HEADER_H
