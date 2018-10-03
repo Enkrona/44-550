3f4f8c44b2f91fa6b9a3bebd04c2aa38c8665bb5
Brad Taylor <brad@enkrona.net>
2018-10-03 14:12:05 -0400

    Threads stuff

--- threads.c
+++ threads.c
@@ -11,14 +11,23 @@
 
 // must be in the form of function pointer
 
-void do_a_thing(void * my_func(void));
 
-int main(int argc, cahr* argv [])
+void do_a_thing(void (*my_func) ());
+void foo();
+void bar();
+
+void do_another_thing(int (*my_func) (int a));
+int square(int x);
+int cube(int x);
+
+
+int main(int argc, char * argv [])
 {
 	// WIP
 	do_a_thing(foo);
 	do_a_thing(bar);
-	
+	do_another_thing(square);
+	do_another_thing(cube);
 	return 0;
 
 }
@@ -35,7 +44,22 @@ void bar()
 	printf("YARRRRRRRRR\n");
 }
 
-void do_a_thing(void * my_func(void))
+void do_a_thing(void (* my_func) ())
 {
+	my_func();
+}
 
+void do_another_thing(int(*my_func) (int a))
+{
+	my_func(4);
+}
+
+int square(int x)
+{
+	return x*x;
+}
+
+int cube(int x)
+{
+	return x*x*x;
 }
