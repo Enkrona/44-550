7ff2f22c8bb8bc3718cb821365d8e1f694e4bcaa
Brad Taylor <brad@enkrona.net>
2018-12-05 14:46:42 -0500

    Automate

--- bashstuffs/automate.sh
+++ bashstuffs/automate.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+
 # some stuff possibly - automate things
 #wget nwmissouri.edu
 
 
-#  Google Chrome - opening github & canvas - and now runs in the background of that shell & errors are output to oblivion 
-
+#  Google Chrome - opening github & canvas - and now runs in the background of that shell 
+#  	AND errors are output to oblivion
+ 
+###############  START WINDOWS SPECIFIC COMMANDS #######
 /c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe --new-window https://github.com 2> /dev/null &
 /c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe https://nwmissouri.instructure.com/login/ldap --new-window 2> /dev/null &
 
-/c/Program\ Files\ \(x86\)/Notepadd\+\+/
+#  Opens notepad - The one true IDE 
+/c/Program\ Files\ \(x86\)/Notepadd\+\+/notepad\+\+.exe
+###############  END WINDOWS SECTION ###############
 
+
+###############  START LINUX VM SECTION ############
 #  Using READ - to read the repo URL in to git 
 echo "What repo would you like to clone?"
 read repoURL
@@ -24,3 +31,5 @@ echo "Please input comment for the commit"
 read commitComment
 gcmsg "$commitComment"
 gp 
+
+############## END LINUX VM SECTION ################
